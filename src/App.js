eff34ffd41f8c887cadf2eb222da38855f464371
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2024-02-22 13:47:54 -0500

    initial commit, desktop styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import fklogo from './assets/FULLKITLOGO.png'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="landingPage">
+        <div className="missionContainer">
+          <p>
+            U.S.-based collective based in NYC/LA/TX. We offer{' '}
+            <span>CUSTOM MERCHANDISE</span>, <span>CREATIVE DIRECTION</span>,
+            and <span>MUSIC SUPERVISION</span>.
+          </p>
+        </div>
+        <div className="logoContainer">
+          <img src={fklogo} className="logo" alt="full kit logo" />
+        </div>
+        <div className="linksContainer">
+          <a
+            className="socialLink"
+            href="mailto:thelads@fullkitwnkrs.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            EMAIL
+          </a>
+          <a
+            className="socialLink"
+            href="https://www.instagram.com/fullkitwnkrs"
+            rel="noreferrer"
+            target="_blank"
+          >
+            INSTAGRAM
+          </a>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
